f9b9af8e83aa6b1ea5c66d9adca736c1a016928d
StellaSam <44140078+StellaSam@users.noreply.github.com>
2019-07-15 10:36:07 -0400

    Delete CheckIfBase.py

--- CheckIfBase.py
+++
@@ -1,7 +0,0 @@
-def checkIfBase(mResource, modelBase):
-	fBase=False
-
-	if ((mResource == modelBase) or (len(mResource) > len(modelBase) and (mResource.find(modelBase) != -1)) or (len(modelBase) > len(mResource) and (modelBase.find(mResource) != -1))):
-		fBase=True
-
-	return(fBase)
