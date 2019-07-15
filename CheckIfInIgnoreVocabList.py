3223704b6e5d59f606a6ce01c460856012cb2c0d
StellaSam <44140078+StellaSam@users.noreply.github.com>
2019-07-15 10:36:25 -0400

    Delete CheckIfInIgnoreVocabList.py

--- CheckIfInIgnoreVocabList.py
+++
@@ -1,8 +0,0 @@
-def inIgnoreVocabList(mResource, ignoreVocabs_Dict):
-	fIgnoreVocab=False
-
-	for key,vocab in ignoreVocabs_Dict.items():	
-		if (vocab.find(mResource) != -1) or ((len(mResource) > len(vocab)) and (mResource.find(vocab[1:len(vocab)-1]) != -1)):
-			fIgnoreVocab=True
-
-	return(fIgnoreVocab)
